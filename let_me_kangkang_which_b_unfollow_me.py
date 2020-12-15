533db204848f11340170048908ac1760ffee7e53
Chen Xu <xuchen.prog@bytedance.com>
2020-12-15 19:01:13 +0800

    update

--- let_me_kangkang_which_b_unfollow_me.py
+++ let_me_kangkang_which_b_unfollow_me.py
@@ -9,7 +9,7 @@ from pync import notify
 # ugly, but it works
 
 logger = logging.getLogger("fan_tracer")
-logging.basicConfig(filename='bin/resource/fan_tracer.log', filemode='a',
+logging.basicConfig(filename='/Users/bytedance/bin/resource/fan_tracer.log', filemode='a',
                     level=logging.INFO, format = '%(asctime)s - %(levelname)s: %(message)s',\
                      datefmt = '%m/%d/%Y %H:%M:%S' )
 
@@ -53,7 +53,7 @@ def handle_unfollower(c, un_follower):
             c.execute(f"update users set (name,used_name,still_follower) = (?,?,?) where uid = {int(follower)}",
                         (name,",".join(used_name),0))
         else:
-            c.execute("update users set still_follower = 0 where uid = ? ", int(follower))
+            c.execute("update users set still_follower = 0 where uid = ? ", (int(follower),))
         unfollower_name.append(name)
     return ",".join(unfollower_name)
 
@@ -68,8 +68,9 @@ def handle_raw_follower(c, follower_raw, new_follower):
             c.execute("select * from users where uid = ?", (follower[0],))
             follower_info = c.fetchall()[0]
             if follower_info[1] != follower[1]:
+                logger.info(f'Name changed: {follower_info[1]} -> {follower[1]}')
                 used_name = follower_info[2].split(',')
-                used_name.append(follower[1])
+                used_name.append(follower_info[1])
                 used_name = ",".join(used_name)
                 c.execute("update users set (name, used_name) = (?,?) where uid = ?", (follower[1], used_name, follower[0]))
 
@@ -97,7 +98,7 @@ def send_mail(new_unfollow_name):
 if __name__ == "__main__":
     try:
         # resource_dir = os.getcwd()
-        with open('bin/resource/xc_info.json') as f:
+        with open('/Users/bytedance/bin/resource/xc_info.json') as f:
             xc_info = ujson.load(f)
         uid = xc_info.get('uid')
         sessdata = xc_info.get('sessdata')
@@ -106,7 +107,7 @@ if __name__ == "__main__":
         resp = bi.user.get_followers(uid=uid, verify=verify)
         num = len(resp)
 
-        conn = sqlite3.connect('bin/resource/followers.db')
+        conn = sqlite3.connect('/Users/bytedance/bin/resource/followers.db')
         c = conn.cursor()
 
         msg = ""
@@ -117,7 +118,7 @@ if __name__ == "__main__":
 
         if new_unfollow:
             new_unfollow_name = handle_unfollower(c, new_unfollow)
-            msg += f'今日取关: {new_unfollow_name}'
+            msg += f'今日取关: {new_unfollow_name}\n'
             try:
                 send_mail(new_unfollow_name)
             except Exception as e:
@@ -131,7 +132,7 @@ if __name__ == "__main__":
             write_into_db(c, followers, new_follow, new_unfollow, num)
             logger.info('Cronjob success, check your email')
 
-            notify(msg, title= 'b站粉丝变动', appIcon="bin/resource/bilibili.png", open='https://www.bilibili.com')
+            notify(msg, title= 'b站粉丝变动', appIcon="/Users/bytedance/bin/resource/bilibili.png", open='https://www.bilibili.com')
 
         else:
             logger.info('Cronjob success, nothing has changed')
